0cd0cad67333448230571f856d9a6bfd97935bc5
mesumitkumarsk <144142094+mesumitkumarsk@users.noreply.github.com>
2024-10-25 23:25:07 +0530

    commit changes

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,13 +1,10 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
-import { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Container, Typography, TextField, Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { TextDecrypt } from "../content/TextDecrypt";
 import Swal from 'sweetalert2';
-
 import emailjs from '@emailjs/browser';
-
 import './Contact.css';
 
 const useStyles = makeStyles((theme) => ({
@@ -25,81 +22,110 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-
-
 export const Contact = () => {
   const classes = useStyles();
   const greetings = "Say hello.";
-
   const form = useRef();
+
+  // State variables to hold the form input values
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  // Handle change to update form data
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_8bezxog', 'template_jmsk313', form.current, 'knwNTK4YU4K30HYMd')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
+    // Validate form inputs
+    if (!formData.name || !formData.email || !formData.message) {
+      Swal.fire({
+        icon: 'warning',
+        title: 'Please fill in all fields before sending the message',
+        position: 'center',
+        showConfirmButton: false,
+        timer: 1500
       });
-    Swal.fire({
-      position: 'center',
-      icon: 'success',
-      title: 'You have sent an email!',
-      showConfirmButton: false,
-      timer: 1500
-    })
-    e.target.reset()
+      return;
+    }
+
+      emailjs.sendForm('service_3tvsfj5', 'template_wzwoe6s', form.current, 'xui86eurGDZC5Ve-w')
+      .then((result) => {
+        console.log(result.text);
+        Swal.fire({
+          position: 'center',
+          icon: 'success',
+          title: 'You have sent an email!',
+          showConfirmButton: false,
+          timer: 1500
+        });
+        setFormData({ name: '', email: '', message: '' });
+      }, (error) => {
+        console.log(error.text);
+      });
   };
 
-
-
-    return (
-      <section id="contact">
-        <Container component="main" className={classes.main} maxWidth="md">
-          <div className="contact">
-            <div className="_form_wrapper">
-              <form ref={form} onSubmit={sendEmail} className={classes.form}>
-                <TextField
-                  id="outlined-name-input"
-                  label="Name"
-                  type="text"
-                  size="small"
-                  variant="filled"
-                  name="name"
-                  className={classes.formfield}
-                />
-                <TextField
-                  id="outlined-password-input"
-                  label="Email"
-                  type="email"
-                  size="small"
-                  variant="filled"
-                  name="email"
-                  className={classes.formfield}
-                />
-                <TextField
-                  id="outlined-password-input"
-                  label="Message"
-                  type="textarea"
-                  size="small"
-                  multiline
-                  minRows={5}
-                  variant="filled"
-                  name="message"
-                  className={classes.formfield}
-                />
-                <button type="submit" value="Send" className="submit-btn">
+  return (
+    <section id="contact">
+      <Container component="main" className={classes.main} maxWidth="md">
+        <div className="contact">
+          <div className="_form_wrapper">
+            <form ref={form} onSubmit={sendEmail} className={classes.form}>
+              <TextField
+                id="outlined-name-input"
+                label="Name"
+                type="text"
+                size="small"
+                variant="filled"
+                name="name"
+                className={classes.formfield}
+                value={formData.name}
+                onChange={handleChange}
+              />
+              <TextField
+                id="outlined-email-input"
+                label="Email"
+                type="email"
+                size="small"
+                variant="filled"
+                name="email"
+                className={classes.formfield}
+                value={formData.email}
+                onChange={handleChange}
+              />
+              <TextField
+                id="outlined-message-input"
+                label="Message"
+                type="textarea"
+                size="small"
+                multiline
+                minRows={5}
+                variant="filled"
+                name="message"
+                className={classes.formfield}
+                value={formData.message}
+                onChange={handleChange}
+              />
+              <button type="submit" value="Send" className="submit-btn">
                 <i className="fas fa-terminal"></i>
-                  <Typography component='span'> Send Message</Typography>
-                </button>
-              </form>
-            </div>
-            <h1 className="contact_msg">
-              <TextDecrypt text={greetings}/>
-            </h1>
+                <Typography component='span'> Send Message</Typography>
+              </button>
+            </form>
           </div>
-        </Container>
-      </section>
+          <h1 className="contact_msg">
+            <TextDecrypt text={greetings} />
+          </h1>
+        </div>
+      </Container>
+    </section>
   );
 };
--- src/components/works/Works.js
+++ src/components/works/Works.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { useState } from "react";
-import { Container } from "@material-ui/core";
+import { Container, Tooltip } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { TextDecrypt } from "../content/TextDecrypt";
 
@@ -10,6 +10,7 @@ import './Works.css';
 // Import ../../assets/recentprojects/
 import App_Project_SS from '../../assets/recentprojects/App_Project_SS.png';
 import resume_pro_ss from '../../assets/recentprojects/resume_pro_ss.png';
+import react_website from '../../assets/recentprojects/react_website.png';
 import Lofo from '../../assets/recentprojects/lofo.png';
 import Startup from '../../assets/recentprojects/startup.png';
 import Lacalle from '../../assets/recentprojects/lacalle.png';
@@ -19,6 +20,20 @@ const useStyles = makeStyles((theme) => ({
     maxWidth: '100vw',
     marginTop: '3em',
     marginBottom: "auto",
+  },
+  projectIcon: {
+    position: 'absolute',
+    bottom: '1rem',
+    right: '1rem',
+    fontSize: '2rem',
+    cursor: 'pointer',
+    transition: 'transform 0.3s ease',
+    color: 'white', // Change the color to white
+  },
+  projectIconHover: {
+    '&:hover': {
+      transform: 'scale(1.1)',
+    },
   },
 }));
 
@@ -31,19 +46,25 @@ export const Works = () => {
       description: `Created a React Native Sign Language App for deaf and mute individuals, featuring interactive lessons and engaging animations. The app aims to enhance communication and make learning sign language fun and accessible.`,
       alter: 'Sign Language App',
       image: `${App_Project_SS}`,
+      link: 'https://github.com/mesumitkumarsk/SIH24',
+      x_icon: 'fab fa-github'
     },
     { 
       id: 2,
       title: 'PACE in the Classroom', 
       description: `Created an educational website, "PACE in the Classrooms," to help teachers and students access NASA's PACE satellite data easily. The site includes a chatbot, games, and quizzes to explore topics like ocean color and phytoplankton growth. Our goal is to make learning about oceans fun and engaging for everyone!`,
       alter: 'resume_pro_ss Project',
-      image: `${resume_pro_ss}`,
+      image: `${react_website}`,
+      link: 'https://github.com/mesumitkumarsk/PACE-IN-CLASSROOMs',
+      x_icon: 'fab fa-github'
     },
     {
       id: 3,
       title: 'ATS Friendly Resume Builder', 
       description: `ATS-friendly Resume Builder to help users create professional resumes that easily pass through applicant tracking systems. The tool features customizable templates and user-friendly input forms, ensuring that resumes are well-structured and visually appealing. This project aims to simplify the job application process and enhance users' chances of landing interviews.`,
       image: `${resume_pro_ss}`,
+      link: 'https://github.com/mesumitkumarsk/ats-friendly-resume-builder',
+      x_icon: 'fab fa-github'
     },
   ]);
 
@@ -62,6 +83,16 @@ export const Works = () => {
               <p className="description">
                 { project.description }
               </p>
+              <Tooltip title="Project link" placement="top">
+                <a 
+                  href={project.link} 
+                  target="_blank" 
+                  rel="noopener noreferrer" 
+                  className={`${classes.projectIcon} ${classes.projectIconHover}`}
+                >
+                  <i className="fab fa-github"></i>
+                </a>
+              </Tooltip>
             </div>
           </div>
         ))}
